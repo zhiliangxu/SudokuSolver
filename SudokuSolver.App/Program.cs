88763ce6c3c015919b480a1570b23ab4983df7bc
Zhiliang Xu <xu.zl@live.com>
2013-09-30 22:34:24 +0800

    Init checkin

---
+++ SudokuSolver.App/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SudokuSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] grid = {
+                               {0, 0, 0, 0, 0, 0, 0, 6, 7},
+                               {0, 0, 0, 9, 0, 7, 4, 0, 3},
+                               {0, 6, 0, 0, 0, 0, 1, 9, 0},
+                               {1, 0, 0, 0, 7, 3, 0, 0, 0},
+                               {0, 0, 0, 4, 0, 6, 0, 0, 0},
+                               {0, 0, 0, 8, 1, 0, 0, 0, 4},
+                               {0, 2, 1, 0, 0, 0, 0, 5, 0},
+                               {8, 0, 7, 1, 0, 9, 0, 0, 0},
+                               {3, 5, 0, 0, 0, 0, 0, 0, 0}
+                           };
+
+            Console.WriteLine("Input:");
+            SudokuSolver.PrintSudokuGame(grid);
+
+            bool solved = SudokuSolver.Solve(grid);
+            Console.WriteLine();
+            Console.WriteLine("Solved: {0}.", solved);
+
+            Console.WriteLine("Solution:");
+            SudokuSolver.PrintSudokuGame(grid);
+        }
+    }
+}
